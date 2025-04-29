81a7f3be98c66baf2b52667590959e28af03511f
Tanu882 <155938556+Tanu882@users.noreply.github.com>
2025-04-29 21:48:36 +0530

    Add files via upload

---
+++ SAAS Project/script.js
@@ -0,0 +1,5 @@
+document.getElementById('contactForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Thanks for contacting us! We’ll get back to you soon.');
+  });
+  
